fbdd018ab9380741590fba79a13dc6434df7234a
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-04-30 09:49:59 -0300

    implemented use case create user

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,11 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) throw new Error("User email already in use");
+
+    return this.usersRepository.create({ name, email });
   }
 }
 

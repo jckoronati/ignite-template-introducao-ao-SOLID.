68ecda0e44875ca98ce00815275fbabfd04aef0f
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-04-30 10:38:21 -0300

    implemented controllers

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,14 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    try {
+      const users = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.send(users);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
